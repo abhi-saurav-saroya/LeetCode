2f20f7505a5aa43d08ccd47ea8a515344713701f
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-01-06 19:31:35 +0530

    Problem 35

---
+++ Arrays/Binary Search/Easy/35) Search Insert Position/O(n).cpp
@@ -0,0 +1,17 @@
+// Linear Search (Sequential)
+// Brute Force
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        if (nums.size() == 0)
+            return 0;
+
+        for(int i = 0; i< nums.size(); i++) {
+            if(nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.size();
+    }
+};
