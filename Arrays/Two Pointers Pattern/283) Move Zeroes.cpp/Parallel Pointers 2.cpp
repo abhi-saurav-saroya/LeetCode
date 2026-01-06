e5878fb69eeb4c1ceed5334bcec7b66e87937a9c
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-01-06 18:57:33 +0530

    Problem 283

---
+++ Arrays/Two Pointers Pattern/283) Move Zeroes.cpp/Parallel Pointers 2.cpp
@@ -0,0 +1,14 @@
+//Using inbuilt swap to avoid manually filling zero in the end
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int k = 0;
+        for(int i=0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[k]);
+                k++;
+            }
+        }
+    }
+};
