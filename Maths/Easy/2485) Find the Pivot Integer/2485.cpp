01eebb894cec13eabcd1e66ef9b4e4e367fa4995
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-01-10 13:36:13 +0530

    Problem 2485

---
+++ Maths/Easy/2485) Find the Pivot Integer/2485.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        for(int x = 1; x <= n; x++) {
+            float sum_left = (x / 2.0) * (1.0 + x);
+            float sum_right = ((n - x + 1.0) / 2.0) * (x + n);
+            if(sum_left == sum_right)
+                return x;
+        }
+        return -1;
+    }
+};
